9e230ba8f555fb639e2d53f645f1bbd9a9a656ee
Elin Larsson <elin.elisabeth.larsson@gmail.com>
2020-01-07 14:55:18 +0100

    added crud functionality to api

--- server/index.js
+++ server/index.js
@@ -2,18 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-// Databasanslutning
-mongoose.connect(
-  "mongodb+srv://disabledbarrel:<password>@vue-express-ebdrj.mongodb.net/test?retryWrites=true&w=majority",
-  {
-    useUnifiedTopology: true
-  }
-);
-
-// Skapa instans av express
+// Skapa instans av express, initierar appen
 const app = express();
 
-// Middleware
+// Middleware för dependencies
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
@@ -21,6 +13,7 @@ app.use(cors());
 // Routes
 const posts = require("./routes/api/posts");
 
+// Dirigerar till posts
 app.use("/api/posts", posts);
 
 // Port för anslutning
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,14 +4,53 @@ const mongodb = require("mongodb");
 const router = express.Router();
 
 // Get Posts
-router.get("/", (req, res) => {
-  res.send("hello there");
+// Refererar till uppsatt route
+router.get("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray()); // Hämtar alla poster från databasen
 });
 
 // Add Posts
+router.post("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send(); // Allt gick ok och post skapades
+});
 
 // Update Posts
+router.put("/:id", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.updateOne(
+    { _id: new mongodb.ObjectID(req.params.id) },
+    {
+      $set: {
+        text: req.body.text
+      }
+    }
+  );
+  res.status(201).send(); // Allt gick ok och post uppdaterades
+});
 
 // Delete Posts
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) }); // skrivsätt som krävs när man använder mongodb
+  res.status(200).send(); // Allt gick ok
+});
+
+// Databasanslutning till MongoDB Atlas och collection
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://disabledbarrel:<password>@vue-express-ebdrj.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true
+    }
+  );
+
+  return client.db("vue-express").collection("posts");
+}
 
 module.exports = router;
